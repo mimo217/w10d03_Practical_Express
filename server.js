f5ff91e0112136ecb85b6a3f19d0240e4422fdc5
mimo217 <mimi.zhou217@gmail.com>
2023-05-28 17:47:22 -0500

    Greeting express application created.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ app.get('/greeting', (req, res) => {
     res.send('<h1>Hello, stranger</h1>')
 })
 
-//dynamic route
 app.get('/greeting/:name', (req, res) => {
     const name = req.params.name
     res.send(`Hola ${name}! It's so good to see you!`)
