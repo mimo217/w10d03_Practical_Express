6792905f9ad678175d02eaa769d48057dc1e8355
mimo217 <mimi.zhou217@gmail.com>
2023-05-28 17:43:42 -0500

    updates

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
-
 const app = express()
+const port = 3000;
 
 app.get('/greeting', (req, res) => {
-    res.send('Hello, stranger')
+    res.send('<h1>Hello, stranger</h1>')
 })
 
-app.listen(3000, () => {
+//dynamic route
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name
+    res.send(`Hola ${name}! It's so good to see you!`)
+})
+
+app.listen(port, () => {
     console.log('Yes, I am listening on port 3000')
 })
